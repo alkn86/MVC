f2b04415ac4ac737b0a2f515531b5f67773101b8
Alexey Konovalenko <alexey.konovalenko@hpe.com>
2016-10-20 16:01:20 +0300

    All project

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public String Index()
+        {
+            return "This is my <b>default<b> action...";
+        }
+
+        public String Welcome(String name, int id = 1)
+        {
+            return HttpUtility.HtmlEncode(String.Format("Welcome {0}, ID is:{1}",name,id));
+        }
+    }
+}
